72659c4a184781a9f4717819d3def5fd98b7149f
Glenn Jones <glenn.jones@pythonanywhere.com>
2020-06-02 17:32:16 +0100

    Some more feedback fixes. by: Glenn

--- themes/pythonanywhere/assets/js/feedback.js
+++ themes/pythonanywhere/assets/js/feedback.js
@@ -6,14 +6,12 @@ window.onmessage = function(e) {
 }
 
 function showDialog() {
-  console.log("showing dialog");
   document.getElementById('id_feedback_container').className='';
   window.frames[0].postMessage("showdialog", "https://www.pythonanywhere.com");
   return false;
 }
 
 $(document).ready(function() {
-  console.log("attaching event");
   $("#id_feedback_link").click(function() {
     showDialog();
   });
