78ed7c9c3418514da06ada31fb5c35d629c54dc2
Giles Thomas <giles@pythonanywhere.com>
2024-07-10 19:28:23 +0000

    Removed old ASGI Django, FastHTML and FastAPI pages, and added redirects for their URLs.  by: Giles

--- flask_app.py
+++ flask_app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return redirect("/pages/")
+
+@app.route('/pages/FastHTML')
+def fasthtml_redirect():
+    return redirect("/pages/ASGICommandLine")
+
+@app.route('/pages/FastAPI')
+def fastapi_redirect():
+    return redirect("/pages/ASGICommandLine")
+
+@app.route('/pages/AsgiDjango')
+def asgi_django_redirect():
+    return redirect("/pages/ASGICommandLine")
